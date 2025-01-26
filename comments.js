39dd0c5af91760f2dd927254459ee5ab52574608
Purist6 <3hwfcdsk@duck.com>
2025-01-26 22:13:31 +0000

    copilot thrid commit AGAIN

--- comments.js
+++ comments.js
@@ -1,26 +1,2 @@
 //create web server
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const server = http.createServer((req, res) => {
-    //set the content type
-    res.setHeader('Content-Type', 'text/html');
-
-    //set the path to the file
-    let filePath = path.join(__dirname, 'comments.html');
-
-    //read the file
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            res.end(data);
-        }
-    });
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server is listening on port 3000');
-});
+var http = require('http');
